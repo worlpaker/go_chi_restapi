7af68ffeb945aa3268c2549e7b0a878f689ab674
Furkan Ozalp <34960716+worlpaker@users.noreply.github.com>
2023-09-22 00:22:48 +0300

    gracefully shut down the server

--- api/server/server.go
+++ api/server/server.go
@@ -3,11 +3,17 @@ package server
 import (
 	"backend/api/handlers"
 	"backend/api/routers"
+	"backend/config"
 	"backend/database"
 	"backend/database/pqdb"
+	"context"
 	"database/sql"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -34,11 +40,42 @@ func NewServer(r *chi.Mux, db *sql.DB) *routers.Server {
 	return s
 }
 
-// Start to server
-func Start(port string) error {
+// gracefulShutDown gracefully shuts down the HTTP server.
+func gracefulShutDown(serverStopCh <-chan os.Signal, server *http.Server) {
+	<-serverStopCh
+	log.Println("Shutting down the server...")
+	// Create a context with a timeout to allow existing connections to finish
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// Shutdown the server gracefully
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Error during server shutdown: %v\n", err)
+	}
+	log.Println("Server gracefully shut down")
+}
+
+// Start an HTTP server with the provided server configuration.
+func Start() {
 	r, d := NewAuth()
 	s := NewServer(r, d)
 	s.SetupRouters()
-	log.Println("API Listen ON", port)
-	return http.ListenAndServe(port, s.Router)
+
+	server := &http.Server{
+		Addr:    config.ServerPort,
+		Handler: s.Router,
+	}
+
+	// Create a channel to listen for OS signals
+	serverStopCh := make(chan os.Signal, 1)
+	signal.Notify(serverStopCh, syscall.SIGINT, syscall.SIGTERM)
+
+	// Start the server in a separate goroutine
+	go func() {
+		log.Println("Server is listening on", config.ServerPort)
+		if err := server.ListenAndServe(); err != http.ErrServerClosed {
+			log.Fatalf("Error: %v\n", err)
+		}
+	}()
+	gracefulShutDown(serverStopCh, server)
 }
--- cmd/main.go
+++ cmd/main.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"backend/api/server"
-	"backend/config"
 )
 
 func main() {
-	panic(server.Start(config.ServerPort))
+	server.Start()
 }
